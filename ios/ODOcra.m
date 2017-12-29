48c85c3242756f082ed6e7ccffab3bed80e2eb99
denissb <spawnenius@gmail.com>
2017-12-29 20:34:18 +0100

    fix: using a hex representation for key and timestamp on iOS

--- ios/ODOcra.m
+++ ios/ODOcra.m
@@ -31,33 +31,58 @@ RCT_EXPORT_METHOD(generateOcra:(NSString*) ocraSuite
         sessionInformation = nil;
     }
     
-    if(timeStamp.length==0){
-        timeStamp = [ODOcra getTimeReference];
+    NSString *timeStampHex = nil;
+    long timeStampLong = 0;
+    
+    // 1. Get timestamp or convert given value to long
+    if (timeStamp.length==0) {
+        timeStampLong = (long)(NSTimeInterval)([[NSDate date] timeIntervalSince1970]);
+    } else {
+        timeStampLong = [timeStamp longLongValue];
     }
-
+    
+    // 2. Round timestamp value
+    long timeStampRounded = [self getTimeReference:timeStampLong];
+    // 3. Convert timestamp to hex
+    timeStampHex = [NSString stringWithFormat:@"0x%lX", timeStampRounded];
+    
+    // Key needs to be converted to hex
+    NSString *ocraKey = [self hexFromString:key];
+    
     //Do some stuff
     NSError *error;
-   
-    NSString *ocra = [OCRA generateOCRAForSuite:ocraSuite key:key counter:counter question:question password:password sessionInformation:sessionInformation timestamp:timeStamp error:&error];
     
-    if(error!=nil){
-         reject(@"OcraSDK", error.localizedDescription, error);
-    }else{
+    NSString *ocra = [OCRA generateOCRAForSuite:ocraSuite key:ocraKey counter:counter question:question password:password sessionInformation:sessionInformation timestamp:timeStampHex error:&error];
+    
+    if (error!=nil){
+        reject(@"OcraSDK", error.localizedDescription, error);
+    } else {
         resolve(ocra);
     }
-    
 }
 
-+(NSString*)getTimeReference{
-    
-    long timeStamp = [[NSDate date] timeIntervalSince1970];
+- (long)getTimeReference:(long)timeStamp
+{
     long roundingFactor = 1000 * 60 * 5;
-    return [NSString stringWithFormat:@"%lu",(timeStamp/roundingFactor)];
+    return timeStamp/roundingFactor;
 }
 
-
-
-
+- (NSString *)hexFromString:(NSString *)str
+{
+    NSUInteger len = [str length];
+    unichar *chars = malloc(len * sizeof(unichar));
+    [str getCharacters:chars];
+    
+    NSMutableString *hexString = [[NSMutableString alloc] init];
+    
+    for(NSUInteger i = 0; i < len; i++ )
+    {
+        // [hexString [NSString stringWithFormat:@"%02x", chars[i]]]; /*previous input*/
+        [hexString appendFormat:@"%02x", chars[i]]; /*EDITED PER COMMENT BELOW*/
+    }
+    free(chars);
+    
+    return hexString;
+}
 
 @end
-  
